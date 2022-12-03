7d76e0db3f39fe00bf3bbd7ea8e65367c5e41afa
Ignacio Bergantinos <ignacio.bergantinos@morado.app>
2022-12-03 13:50:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import datetime
 
 
 tokens = { 
-  "NYT__" : ["0xfdff7a8eda6a3739132867f989be4bf84e803c15","https://coinmarketcap.com/dexscan/bsc/0x6dcb370b61b9ee192082a1c42fa994f767916754", 190000, 480, 1000],
-  "FLOKY" : ["0x4a01d24aa894530f83fa3764c584fc75885762b4", "https://coinmarketcap.com/dexscan/bsc/0x331b77297fa9acfbf2d73aa7feb693a335efb994", 825171.882619393,200, 1000],
+  "NYT__" : ["0xfdff7a8eda6a3739132867f989be4bf84e803c15","https://coinmarketcap.com/dexscan/bsc/0x6dcb370b61b9ee192082a1c42fa994f767916754", 190000, 480, 5000],
+  "FLOKY" : ["0x4a01d24aa894530f83fa3764c584fc75885762b4", "https://coinmarketcap.com/dexscan/bsc/0x331b77297fa9acfbf2d73aa7feb693a335efb994", 825171.882619393,200, 2000],
   "CHRIS" : ['0xc51a7658022bae406900536525877a6dec99c34c',"https://coinmarketcap.com/dexscan/bsc/0x824147f9da7185aeb7e8aa27b102dbef6ca9d690", 48611.243960888,100 , 500],
   "MEC__" : ["0x4b264f0b2dcbe5a63fb8734d76a644236680ce2d","https://coinmarketcap.com/dexscan/bsc/0xd227f7c2a5b152c3757d1324b31b8fe414fe77ca", 1840000, 41, 300],
   "QATAR" : ['0x238f5cc8bd082895d1f832cef967e7bb7ba4f992',"https://coinmarketcap.com/dexscan/bsc/0xf1d85613b7cb69460e9cd8565f9793448ecbc83c", 35680.6657466065048718,5, 50],
@@ -27,10 +27,10 @@ def main():
   
   total = getSumOfPrices(prices, hora)
 
-  takeProfitAlert()
-  trailingStopAlert()
- 
+  takeProfitAlert(prices, hora) 
 
+  # trailingStopAlert()
+ 
 
 def getPrices():
   prices = {}
@@ -46,7 +46,6 @@ def getPrices():
 
   return prices
 
-
 def getSumOfPrices(prices, hora):
   total = 0
   message = ""
@@ -56,7 +55,7 @@ def getSumOfPrices(prices, hora):
     urlToken = tokens[symbol][1]
     tokensQuantity = tokens[symbol][2]
     initialInvestment = tokens[symbol][3]
-    expectedReturn = tokens[symbol][3]
+    expectedReturn = tokens[symbol][4]
 
     try: 
         price = prices[urlToken]
@@ -79,12 +78,29 @@ def getSumOfPrices(prices, hora):
 
   return total
 
+def takeProfitAlert(prices, hora):
+  for symbol in tokens:
+    contract = tokens[symbol][0]
+    urlToken = tokens[symbol][1]
+    tokensQuantity = tokens[symbol][2]
+    initialInvestment = tokens[symbol][3]
+    expectedReturn = tokens[symbol][4]
 
-def takeProfitAlert():
-  return true
+    price = prices[urlToken]
+    money = round(price * tokensQuantity,2)
+
+
+    title = hora + " - " + symbol + " - $" + str(money)
+
+    message = "TAKE PROFIT!!!" + "\n" + "Initial investment: $" + str(initialInvestment) + "\n" + "Expected return: $" + str(expectedReturn)
+
+    if (money > expectedReturn):
+      sendNotificationPhone(title, message, "takeProfit")
+
 
 def trailingStopAlert():
-  return true
+  #if price drop x%. (local storage?.. probably)
+  return True
 
 def getTime():
   
