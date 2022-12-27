21619ae3d33adb7ca33a5ab89fba35f779cee8db
Ignacio Bergantinos <ignacio.bergantinos@morado.app>
2022-12-27 20:40:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -159,7 +159,6 @@ def getTime():
 def sendNotificationPhone(title, message, type):
     url = "https://wirepusher.com/send?id=PPkWmps6F&title=" + title + "&message=" + message + "&type=" + type
     response = requests.get(url)
-    print(response.content)
 
 
 def getCoinMarketCapPrice(url):
