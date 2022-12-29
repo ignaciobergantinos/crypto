4d74353981bf54198026e63426ee91a074938b7c
Ignacio Bergantinos <ignacio.bergantinos@morado.app>
2022-12-28 22:33:08 -0300

    update

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from lxml import etree
 # from fake_useragent import UserAgent
 import datetime, time
+import aws
 
 # TODO ADD initial investment sum in title
 
@@ -33,6 +34,12 @@ def main():
     total = getSumOfPrices(prices, hora)
 
     takeProfitAlert(prices, hora)
+
+    dynamoDBClient = aws.dynamodb()
+    dynamoDBClient.getItem("cryptoTrades", '0xfdff7a8eda6a3739132867f989be4bf84e803c15')
+    print(dynamoDBClient.getAccountAddress(), dynamoDBClient.getBuyPrice(), dynamoDBClient.getBuyPrice(),
+          dynamoDBClient.getBuyTax(), dynamoDBClient.getLastPrice(), dynamoDBClient.getSellTax(),
+          dynamoDBClient.getTokensAmount())
 
 
 # ---------------- testing
